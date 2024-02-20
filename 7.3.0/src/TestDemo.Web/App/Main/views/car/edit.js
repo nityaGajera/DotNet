d64a3a536980089dcb838b8897d11247d9669b94
HITESH\HITESH GAJERA <nityagajera24@gmail.com>
2024-02-20 15:12:34 +0530

    nitya 20/2/24

--- 7.3.0/src/TestDemo.Web/App/Main/views/car/edit.js
+++ 7.3.0/src/TestDemo.Web/App/Main/views/car/edit.js
@@ -1,1 +1,44 @@
-﻿
+﻿(function () {
+    var myApp = angular.module('app');
+    myApp.controller('app.views.car.edit', [
+        '$scope', '$uibModalInstance', 'abp.services.app.car', 'id',
+        function ($scope, $uibModalInstance, carService, id) {
+            var vm = this;
+            vm.car = {};
+
+            function init() {
+                debugger;
+                if (id == undefined) {
+
+                } else {
+                    getCarbyid();
+                }
+            }
+            init();
+
+            function getCarbyid() {
+                carService.getCarbyid({
+                    id: id
+                }).then(function (result) {
+                    debugger;
+                    vm.car = result.data;
+                    console.log(vm.car);
+                });
+            }
+
+            vm.cancel = function () {
+                $uibModalInstance.close();
+            }
+            vm.save = function () {
+
+                carService.updateCar(vm.car)
+                    .then(function () {
+                        abp.notify.info(App.localize('SavedSuccessfully'));
+                        $uibModalInstance.close();
+                    });
+
+            };
+
+        }
+    ]);
+})();
